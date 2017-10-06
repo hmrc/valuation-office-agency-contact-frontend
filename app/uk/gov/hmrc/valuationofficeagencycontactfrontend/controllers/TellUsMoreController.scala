e8b1bc5c827c86941ef3afd11b5d2f626fc58e73
calinursaciuc <calin.ursaciuc@digital.hmrc.gov.uk>
2017-10-06 10:57:44 +0100

    VOAC-26: WIP

---
+++ app/uk/gov/hmrc/valuationofficeagencycontactfrontend/controllers/TellUsMoreController.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.valuationofficeagencycontactfrontend.controllers
+
+import javax.inject.Inject
+
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.connectors.DataCacheConnector
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.controllers.actions._
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.{FrontendAppConfig, Navigator}
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.forms.TellUsMoreForm
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.identifiers.TellUsMoreId
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.models.{Mode, TellUsMore}
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.utils.UserAnswers
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.views.html.tellUsMore
+
+import scala.concurrent.Future
+
+class TellUsMoreController @Inject()(appConfig: FrontendAppConfig,
+                                                  override val messagesApi: MessagesApi,
+                                                  dataCacheConnector: DataCacheConnector,
+                                                  navigator: Navigator,
+                                                  getData: DataRetrievalAction,
+                                                  requireData: DataRequiredAction) extends FrontendController with I18nSupport {
+
+  def onPageLoad(mode: Mode) = (getData andThen requireData) {
+    implicit request =>
+      val preparedForm = request.userAnswers.tellUsMore match {
+        case None => TellUsMoreForm()
+        case Some(value) => TellUsMoreForm().fill(value)
+      }
+      Ok(tellUsMore(appConfig, preparedForm, mode))
+  }
+
+  def onSubmit(mode: Mode) = (getData andThen requireData).async {
+    implicit request =>
+      TellUsMoreForm().bindFromRequest().fold(
+        (formWithErrors: Form[TellUsMore]) =>
+          Future.successful(BadRequest(tellUsMore(appConfig, formWithErrors, mode))),
+        (value) =>
+          dataCacheConnector.save[TellUsMore](request.sessionId, TellUsMoreId.toString, value).map(cacheMap =>
+            Redirect(navigator.nextPage(TellUsMoreId, mode)(new UserAnswers(cacheMap))))
+      )
+  }
+}
---
+++ test/uk/gov/hmrc/valuationofficeagencycontactfrontend/forms/TellUsMoreFormSpec.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.valuationofficeagencycontactfrontend.forms
+
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.forms.behaviours.FormBehaviours
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.models.TellUsMore
+
+class TellUsMoreFormSpec extends FormBehaviours {
+
+  val validData: Map[String, String] = Map(
+    "message" -> "value 1"
+  )
+
+  val form = TellUsMoreForm()
+
+  "TellUsMore form" must {
+    behave like questionForm(TellUsMore("value 1"))
+
+    behave like formWithMandatoryTextFields("message")
+  }
+}
