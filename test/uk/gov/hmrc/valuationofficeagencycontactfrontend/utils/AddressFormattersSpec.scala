44b9119ab7613694a2f80af375591f33c07cd27e
calinursaciuc <calin.ursaciuc@digital.hmrc.gov.uk>
2017-10-13 11:04:11 +0100

    FORMATTING: WIP

---
+++ test/uk/gov/hmrc/valuationofficeagencycontactfrontend/utils/AddressFormattersSpec.scala
@@ -0,0 +1,26 @@
+package uk.gov.hmrc.valuationofficeagencycontactfrontend.utils
+
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.SpecBase
+
+class AddressFormattersSpec extends SpecBase {
+
+  "Address Formatter" must {
+
+    "Given a complete Council Tax Address it should generate a formatted string using the given interstitial" in {
+
+    }
+
+    "Given an incomplete Council Tax Address it should create formatted string using the given interstitial" in {
+
+    }
+
+    "Given a Council Tax Address with elements that have too many spaces it should generate a formatted string using the given interstitial" in {
+
+    }
+
+    "Given a Sequence with three strings insert the interstitials" in {
+
+    }
+
+  }
+}
