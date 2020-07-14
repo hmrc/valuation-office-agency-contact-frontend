ce3b8d1eddd965373780217a4d6d116a4919f8d8
Stephen Goddard <37335852+Stephen-Goddard@users.noreply.github.com>
2020-07-14 10:39:15 +0100

    VOA-2080 Change providing lettings page to GDA framework

--- test/uk/gov/hmrc/valuationofficeagencycontactfrontend/controllers/ValuationAdviceControllerSpec.scala
+++ test/uk/gov/hmrc/valuationofficeagencycontactfrontend/controllers/ValuationAdviceControllerSpec.scala
@@ -18,18 +18,20 @@ package uk.gov.hmrc.valuationofficeagencycontactfrontend.controllers
 
 import play.api.test.Helpers._
 import uk.gov.hmrc.valuationofficeagencycontactfrontend.utils.MessageControllerComponentsHelpers
-import uk.gov.hmrc.valuationofficeagencycontactfrontend.views.html.valuationAdvice
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.views.html.{valuationAdvice => valuation_advice}
 
 class ValuationAdviceControllerSpec extends ControllerSpecBase {
 
+  def valuationAdvice = app.injector.instanceOf[valuation_advice]
+
   "Valuation Advice Controller" must {
     "return 200 for a GET" in {
-      val result = new ValuationAdviceController(frontendAppConfig, messagesApi, MessageControllerComponentsHelpers.stubMessageControllerComponents).onPageLoad()(fakeRequest)
+      val result = new ValuationAdviceController(frontendAppConfig, messagesApi, valuationAdvice, MessageControllerComponentsHelpers.stubMessageControllerComponents).onPageLoad()(fakeRequest)
       status(result) mustBe OK
     }
 
     "return the correct view for a GET" in {
-      val result = new ValuationAdviceController(frontendAppConfig, messagesApi, MessageControllerComponentsHelpers.stubMessageControllerComponents).onPageLoad()(fakeRequest)
+      val result = new ValuationAdviceController(frontendAppConfig, messagesApi, valuationAdvice, MessageControllerComponentsHelpers.stubMessageControllerComponents).onPageLoad()(fakeRequest)
       contentAsString(result) mustBe valuationAdvice(frontendAppConfig)(fakeRequest, messages).toString
     }
 
