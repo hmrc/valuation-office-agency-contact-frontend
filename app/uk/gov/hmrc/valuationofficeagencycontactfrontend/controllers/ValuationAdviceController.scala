853248310a992f219a7d5a4f5127b530c7dc8558
Stephen Goddard <37335852+Stephen-Goddard@users.noreply.github.com>
2020-07-14 10:36:58 +0100

    VOA-2080 Change tests for GDS framework

--- app/uk/gov/hmrc/valuationofficeagencycontactfrontend/controllers/ValuationAdviceController.scala
+++ app/uk/gov/hmrc/valuationofficeagencycontactfrontend/controllers/ValuationAdviceController.scala
@@ -21,11 +21,12 @@ import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
 import uk.gov.hmrc.valuationofficeagencycontactfrontend.FrontendAppConfig
-import uk.gov.hmrc.valuationofficeagencycontactfrontend.views.html.valuationAdvice
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.views.html.{valuationAdvice => valuation_advice}
 
 @Singleton
 class ValuationAdviceController @Inject()(val appConfig: FrontendAppConfig,
                                           override val messagesApi: MessagesApi,
+                                          valuationAdvice: valuation_advice,
                                           cc: MessagesControllerComponents
                                          ) extends FrontendController(cc) with I18nSupport {
 
