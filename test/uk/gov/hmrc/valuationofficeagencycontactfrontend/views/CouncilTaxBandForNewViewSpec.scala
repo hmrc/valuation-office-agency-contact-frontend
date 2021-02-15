a9a09d30cad16caf19c03e5a26b9e02672b32173
DursunGundogan <78355528+DursunGundogan@users.noreply.github.com>
2021-02-15 08:49:38 +0000

    VOA-2592 Changed the H2 text to "Other help on GOV.UK"

---
+++ test/uk/gov/hmrc/valuationofficeagencycontactfrontend/views/CouncilTaxBandForNewViewSpec.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.valuationofficeagencycontactfrontend.views
+
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.models.NormalMode
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.views.behaviours.ViewBehaviours
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.views.html.{councilTaxBandForNew => council_tax_band_for_new}
+
+class CouncilTaxBandForNewViewSpec extends ViewBehaviours {
+
+  def councilTaxBandForNew = app.injector.instanceOf[council_tax_band_for_new]
+
+  def view = () => councilTaxBandForNew(frontendAppConfig)(fakeRequest, messages)
+
+  "Council Tax Band For A New Property view" must {
+    behave like normalPage(view, "councilTaxBandForNew", "title", "heading",
+      "subheading", "p1.part1", "p1.url", "p1.part2", "li1", "li2", "li3", "p2", "p3.part1", "p3.part2", "p3.url",
+      "p4.part1", "p4.url", "p4.part2")
+
+    "has a link marked with site.back leading to the Council Tax Band Too High Page" in {
+      val doc = asDocument(view())
+      val backlinkText = doc.select("a[class=govuk-back-link]").text()
+      backlinkText mustBe messages("site.back")
+      val backlinkUrl = doc.select("a[class=govuk-back-link]").attr("href")
+      backlinkUrl mustBe uk.gov.hmrc.valuationofficeagencycontactfrontend.controllers.routes.CouncilTaxSubcategoryController.onPageLoad(NormalMode).url
+    }
+  }
+}
