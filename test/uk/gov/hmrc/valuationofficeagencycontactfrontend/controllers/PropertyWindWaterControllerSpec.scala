339637bc791e1e3613b75e6f997e197f091223df
DursunGundogan <78355528+DursunGundogan@users.noreply.github.com>
2021-02-16 10:08:32 +0000

    VOA-2596 Create CF page: 'The property is protected from wind and water'

---
+++ test/uk/gov/hmrc/valuationofficeagencycontactfrontend/controllers/PropertyWindWaterControllerSpec.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.valuationofficeagencycontactfrontend.controllers
+
+import play.api.test.Helpers._
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.controllers.actions.DataRetrievalAction
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.utils.MessageControllerComponentsHelpers
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.views.html.{propertyWindWatertight => property_wind}
+
+import javax.inject.Singleton
+
+@Singleton
+class PropertyWindWaterControllerSpec extends ControllerSpecBase {
+
+  def propertyWindWatertight = app.injector.instanceOf[property_wind]
+
+  def controller(dataRetrievalAction: DataRetrievalAction = getEmptyCacheMap) =
+    new PropertyWindWaterController(frontendAppConfig, messagesApi,propertyWindWatertight,
+      MessageControllerComponentsHelpers.stubMessageControllerComponents)
+
+  "Property Wind And Water Controller" must {
+    "return the correct view for a GET" in {
+      val result = controller().onPageLoad()(fakeRequest)
+      contentAsString(result) mustBe propertyWindWatertight(frontendAppConfig)(fakeRequest, messages).toString
+    }
+  }
+}
