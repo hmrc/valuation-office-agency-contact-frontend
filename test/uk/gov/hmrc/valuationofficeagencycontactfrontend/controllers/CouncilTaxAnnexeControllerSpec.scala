00e0ca9a829e23997be45bebb4aeac36521e0907
Tevyn Allen <59606793+TevynAllen@users.noreply.github.com>
2021-03-11 13:36:51 +0000

    VOA-2600 & VOA-2605 - Add annexe self contained pages (#311)
    * VOA-2599 Create CF page: 'Has the annexe been added or removed?'
    
    * VOA-2599 Create CF page: 'Has the annexe been added or removed?'
    
    * VOA-2621 - Remove confirmation email
    
    * VOA-2600 & VOA-2605 - Add annexe self contained pages
    
    * Delete files
    
    Co-authored-by: DursunGundogan <78355528+DursunGundogan@users.noreply.github.com>

--- test/uk/gov/hmrc/valuationofficeagencycontactfrontend/controllers/CouncilTaxAnnexeControllerSpec.scala
+++ test/uk/gov/hmrc/valuationofficeagencycontactfrontend/controllers/CouncilTaxAnnexeControllerSpec.scala
@@ -16,33 +16,44 @@
 
 package uk.gov.hmrc.valuationofficeagencycontactfrontend.controllers
 
+import org.mockito.Matchers.any
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar.mock
 import play.api.data.Form
 import play.api.libs.json.JsString
 import play.api.test.Helpers._
 import uk.gov.hmrc.http.cache.client.CacheMap
 import uk.gov.hmrc.valuationofficeagencycontactfrontend.FakeNavigator
-import uk.gov.hmrc.valuationofficeagencycontactfrontend.connectors.FakeDataCacheConnector
-import uk.gov.hmrc.valuationofficeagencycontactfrontend.controllers.actions.{DataRequiredActionImpl, DataRetrievalAction, FakeDataRetrievalAction}
-import uk.gov.hmrc.valuationofficeagencycontactfrontend.forms.{AnnexeForm, EnquiryCategoryForm}
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.connectors.DataCacheConnector
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.controllers.actions._
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.forms.{AnnexeForm, AnnexeSelfContainedForm}
 import uk.gov.hmrc.valuationofficeagencycontactfrontend.identifiers.CouncilTaxAnnexId
 import uk.gov.hmrc.valuationofficeagencycontactfrontend.models.NormalMode
 import uk.gov.hmrc.valuationofficeagencycontactfrontend.utils.MessageControllerComponentsHelpers
 import uk.gov.hmrc.valuationofficeagencycontactfrontend.views.html.{councilTaxAnnex => council_tax_annex}
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.views.html.{annexeSelfContainedEnquiry => annexe_self_contained_enquiry}
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.views.html.{annexeNotSelfContained => annexe_not_self_contained}
 
-class CouncilTaxAnnexControllerSpec extends ControllerSpecBase {
+import scala.concurrent.Future
+
+class CouncilTaxAnnexeControllerSpec extends ControllerSpecBase {
 
   def councilTaxAnnex = app.injector.instanceOf[council_tax_annex]
+  def councilTaxAnnexeSelfContainedEnquiry = app.injector.instanceOf[annexe_self_contained_enquiry]
+  def councilTaxAnnexeNotSelfContained = app.injector.instanceOf[annexe_not_self_contained]
 
-  def onwardRoute = routes.CouncilTaxAnnexController.onPageLoad()
+  val fakeDataCacheConnector = mock[DataCacheConnector]
+
+  def onwardRoute = routes.EnquiryCategoryController.onPageLoad(NormalMode)
 
   def controller(dataRetrievalAction: DataRetrievalAction = getEmptyCacheMap) =
-    new CouncilTaxAnnexController(frontendAppConfig, messagesApi, FakeDataCacheConnector, new FakeNavigator(desiredRoute = onwardRoute),
-      dataRetrievalAction, new DataRequiredActionImpl(ec), councilTaxAnnex, MessageControllerComponentsHelpers.stubMessageControllerComponents)
+    new CouncilTaxAnnexeController(frontendAppConfig, messagesApi, fakeDataCacheConnector, new FakeNavigator(desiredRoute = onwardRoute),
+      dataRetrievalAction, new DataRequiredActionImpl(ec), councilTaxAnnex, councilTaxAnnexeSelfContainedEnquiry, councilTaxAnnexeNotSelfContained,
+      MessageControllerComponentsHelpers.stubMessageControllerComponents)
 
   def viewAsString(form: Form[String] = AnnexeForm()) = councilTaxAnnex(frontendAppConfig, form, NormalMode)(fakeRequest, messages).toString
 
-  "CouncilTaxAnnex Controller" must {
-
+  "Council Tax Annex Controller" must {
     "return OK and the correct view for a GET" in {
       val result = controller().onPageLoad(NormalMode)(fakeRequest)
 
@@ -60,6 +71,12 @@ class CouncilTaxAnnexControllerSpec extends ControllerSpecBase {
     }
 
     "redirect to the next page when valid data is submitted" in {
+      when(fakeDataCacheConnector.remove(any[String], any[String]))
+        .thenReturn(Future.successful(true))
+
+      when(fakeDataCacheConnector.save(any, any, any)(any))
+        .thenReturn(Future.successful(CacheMap("council_tax_annexe", Map("council_tax_annexe" -> JsString("bar")))))
+
       val postRequest = fakeRequest.withFormUrlEncodedBody(("value", AnnexeForm.options.head.value))
 
       val result = controller().onSubmit(NormalMode)(postRequest)
@@ -78,7 +95,7 @@ class CouncilTaxAnnexControllerSpec extends ControllerSpecBase {
       contentAsString(result) mustBe viewAsString(boundForm)
     }
 
-    "return eror page if no existing data is found" in {
+    "return error page if no existing data is found" in {
       val result = controller(dontGetAnyData).onPageLoad(NormalMode)(fakeRequest)
       status(result) mustBe SEE_OTHER
     }
@@ -89,6 +106,28 @@ class CouncilTaxAnnexControllerSpec extends ControllerSpecBase {
 
       status(result) mustBe SEE_OTHER
       redirectLocation(result) mustBe Some(onwardRoute.url)
+    }
+
+    "redirect to the next page when valid data is submitted for annexe self contained" in {
+
+      when(fakeDataCacheConnector.save(any, any, any)(any))
+        .thenReturn(Future.successful(CacheMap("annexeSelfContained", Map("annexeSelfContained" -> JsString("bar")))))
+
+      val postRequest = fakeRequest.withFormUrlEncodedBody(("value", AnnexeSelfContainedForm.options.head.value))
+
+      val result = controller().onSelfContainedSubmit()(postRequest)
+
+      status(result) mustBe SEE_OTHER
+    }
+
+    "return a Bad Request and errors when invalid data is submitted for annexe self contained form" in {
+      val postRequest = fakeRequest.withFormUrlEncodedBody(("value", "invalid value"))
+      val boundForm = AnnexeSelfContainedForm().bind(Map("value" -> "invalid value"))
+
+      val result = controller().onSelfContainedSubmit()(postRequest)
+
+      status(result) mustBe BAD_REQUEST
+      contentAsString(result) mustBe councilTaxAnnexeSelfContainedEnquiry(frontendAppConfig, boundForm)(fakeRequest, messages).toString()
     }
   }
 }
---
+++ test/uk/gov/hmrc/valuationofficeagencycontactfrontend/views/AnnexeNotSelfContainedViewSpec.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.valuationofficeagencycontactfrontend.views
+
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.views.behaviours.ViewBehaviours
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.views.html.{annexeNotSelfContained => annexe_not_self_contained}
+
+class AnnexeNotSelfContainedViewSpec extends ViewBehaviours {
+
+  def councilTaxAnnexeNotSelfContained = app.injector.instanceOf[annexe_not_self_contained]
+
+  def view = () => councilTaxAnnexeNotSelfContained(frontendAppConfig)(fakeRequest, messages)
+
+  "Council Tax Bill view" must {
+    behave like normalPage(view, "annexeNotSelfContained", "title", "heading",
+      "p1", "p2", "subheading")
+
+    "has a link marked with site.back leading to the Council Tax annexe self contained Page" in {
+      val doc = asDocument(view())
+      val backlinkText = doc.select("a[class=govuk-back-link]").text()
+      backlinkText mustBe messages("site.back")
+      val backlinkUrl = doc.select("a[class=govuk-back-link]").attr("href")
+      backlinkUrl mustBe uk.gov.hmrc.valuationofficeagencycontactfrontend.controllers.routes.CouncilTaxAnnexeController.onSelfContainedPageLoad().url
+    }
+  }
+
+}
