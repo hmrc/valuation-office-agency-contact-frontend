7b995102e870e636a1899c6b17de59131e7cc0b4
DursunGundogan <78355528+DursunGundogan@users.noreply.github.com>
2021-03-19 17:12:37 +0000

    VOA-2669 Create CF page: 'You can check and challenge your business rates valuation online'

---
+++ test/uk/gov/hmrc/valuationofficeagencycontactfrontend/views/BusinessRatesChangeValuationViewSpec.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.valuationofficeagencycontactfrontend.views
+
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.models.NormalMode
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.views.behaviours.ViewBehaviours
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.views.html.{businessRatesChangeValuation => change_valuation}
+
+class BusinessRatesChangeValuationViewSpec extends ViewBehaviours {
+
+  def businessRatesChangeValuation = app.injector.instanceOf[change_valuation]
+
+  def view = () => businessRatesChangeValuation(frontendAppConfig, NormalMode)(fakeRequest, messages)
+
+  "Business rates change valuation online view" must {
+    behave like normalPage(view, "valuationOnline", "title", "heading",
+      "p1.url", "p1.part1", "p1.part2", "subheading1", "p2", "subheading1", "p3.url", "p3", "p4.url", "p4")
+
+    "has a link marked with site.back leading to the Business Rates change valuation page" in {
+      val doc = asDocument(view())
+      val backlinkText = doc.select("a[class=govuk-back-link]").text()
+      backlinkText mustBe messages("site.back")
+      val backlinkUrl = doc.select("a[class=govuk-back-link]").attr("href")
+      backlinkUrl mustBe uk.gov.hmrc.valuationofficeagencycontactfrontend.controllers.routes.BusinessRatesSubcategoryController.onPageLoad(NormalMode).url
+    }
+  }
+}
+
