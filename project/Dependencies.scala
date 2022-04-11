0ee3c1055f38aba12cd1b2baa8220c7481b89919
Yuriy Tumakha <tumakha@gmail.com>
2022-04-11 11:25:06 +0100

    :warning: Before deployment to production the shuttering service is recommended to close all active sessions.
    VOA-3300 CF Migrate to hmrc-mongo and MongoCacheRepository (#512)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,13 +4,12 @@ import sbt._
 
 object Dependencies {
 
-  private val bootstrapPlayVersion = "5.20.0"
-  private val playFrontendHmrc = "3.5.0-play-28"
+  private val bootstrapPlayVersion = "5.21.0"
+  private val playFrontendHmrc = "3.13.0-play-28"
   private val logbackJsonLoggerVersion = "5.2.0"
   private val httpCachingClientVersion = "9.6.0-play-28"
-  private val simpleReactivemongoVersion = "8.0.0-play-28"
+  private val hmrcMongoVersion = "0.62.0"
   private val playConditionalFormMappingVersion = "1.11.0-play-28"
-  private val playLanguageVersion = "5.1.0-play-28"
   private val httpVerbsVersion = "13.12.0"
   private val commonTextVersion = "1.9"
   private val scalaTestVersion = "3.2.11"
@@ -21,18 +20,17 @@ object Dependencies {
     ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapPlayVersion,
     "uk.gov.hmrc" %% "play-frontend-hmrc" % playFrontendHmrc,
-    "uk.gov.hmrc" %% "simple-reactivemongo" % simpleReactivemongoVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % hmrcMongoVersion,
     "uk.gov.hmrc" %% "logback-json-logger" % logbackJsonLoggerVersion,
     "uk.gov.hmrc" %% "http-caching-client" % httpCachingClientVersion,
     "uk.gov.hmrc" %% "play-conditional-form-mapping" % playConditionalFormMappingVersion,
-    "uk.gov.hmrc" %% "play-language" % playLanguageVersion,
     "uk.gov.hmrc" %% "http-verbs-play-28" % httpVerbsVersion,
     "org.apache.commons" % "commons-text" % commonTextVersion
   )
 
   private val test = Seq(
     "org.scalatest" %% "scalatest" % scalaTestVersion % Test,
-    "com.vladsch.flexmark" % "flexmark-all" % "0.62.2" % Test,
+    "com.vladsch.flexmark" % "flexmark-all" % "0.62.2" % Test, // for scalatest 3.2.x
     "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusPlayVersion % Test,
     "org.scalatestplus" %% "mockito-4-2" % "3.2.11.0" % Test,
     "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0" % Test,
