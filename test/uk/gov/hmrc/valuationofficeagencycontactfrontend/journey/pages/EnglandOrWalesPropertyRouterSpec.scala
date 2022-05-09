cdef61954ed159cefb6107ea7c0c7b56fb3807c9
Yuriy Tumakha <tumakha@gmail.com>
2022-05-09 17:09:20 +0100

    VOA-3326 VOA-3327 Update CF page: `Check and challenge your business rates valuation online` (#515)
    VOA-3326 VOA-3327
    * Update CF page: `Check and challenge your business rates valuation online`
    * added EnglandOrWalesPropertyRouterSpec

---
+++ test/uk/gov/hmrc/valuationofficeagencycontactfrontend/journey/pages/EnglandOrWalesPropertyRouterSpec.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.valuationofficeagencycontactfrontend.journey.pages
+
+import org.scalatest._
+import flatspec._
+import matchers._
+import play.api.libs.json.{JsString, JsValue}
+import uk.gov.hmrc.http.cache.client.CacheMap
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.controllers.routes
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.identifiers.BusinessRatesSubcategoryId
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.models.NormalMode
+import uk.gov.hmrc.valuationofficeagencycontactfrontend.utils.UserAnswers
+
+/**
+ * @author Yuriy Tumakha
+ */
+class EnglandOrWalesPropertyRouterSpec extends AnyFlatSpec with should.Matchers {
+
+  private val emptyUserAnswers = userAnswers(Map())
+
+  "EnglandOrWalesPropertyRouter page" should "have correct previous link" in {
+    EnglandOrWalesPropertyRouter.previousPage(emptyUserAnswers) shouldBe routes.BusinessRatesSubcategoryController.onPageLoad(NormalMode)
+  }
+
+  it should "return correct next page" in {
+    val demolishedPropertyAnswer = userAnswers(Map(BusinessRatesSubcategoryId.toString -> JsString("business_rates_demolished")))
+    EnglandOrWalesPropertyRouter.nextPage(demolishedPropertyAnswer) shouldBe routes.BusinessRatesSubcategoryController.onDemolishedPageLoad()
+  }
+
+  it should "return start page for empty UserAnswers" in {
+    EnglandOrWalesPropertyRouter.nextPage(emptyUserAnswers) shouldBe routes.EnquiryCategoryController.onPageLoad(NormalMode)
+  }
+
+  it should "return correct next page and language" in {
+    val propertyAreaChangesInWalesAnswers = userAnswers(Map(
+      BusinessRatesSubcategoryId.toString -> JsString("business_rates_changes"),
+      EnglandOrWalesPropertyRouter.key -> JsString("wales"),
+    ))
+    EnglandOrWalesPropertyRouter.nextPage(propertyAreaChangesInWalesAnswers) shouldBe routes.BusinessRatesChallengeController.onAreaChangePageLoad()
+    EnglandOrWalesPropertyRouter.nextLang(propertyAreaChangesInWalesAnswers).get.code shouldBe "cy"
+  }
+
+  private def userAnswers(data: Map[String, JsValue]) =
+    new UserAnswers(new CacheMap("cacheId", data))
+
+}
