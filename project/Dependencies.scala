1b244912693049bc159be47cff7de4889f0255ef
Yuriy Tumakha <tumakha@gmail.com>
2022-11-29 14:43:14 +0000

    VOA-3463 CF new section `Future valuation enquiries` with link to 2023 business rates (#532)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,17 +3,17 @@ import sbt._
 
 object Dependencies {
 
-  private val bootstrapPlayVersion = "7.11.0"
-  private val playFrontendHmrc = "3.32.0-play-28"
-  private val hmrcMongoVersion = "0.73.0"
+  private val bootstrapPlayVersion = "7.12.0"
+  private val playFrontendHmrc = "3.34.0-play-28"
+  private val hmrcMongoVersion = "0.74.0"
   private val httpCachingClientVersion = "10.0.0-play-28"
   private val commonsTextVersion = "1.10.0"
 
   // Test dependencies
-  private val scalaTestVersion = "3.2.14"
-  private val scalaTestPlusCheckVersion = "3.2.14.0"
-  private val scalaTestPlusMockitoVersion = "3.2.12.0"
-  private val scalaTestPlusPlayVersion = "5.1.0"
+  private val scalaTestVersion            = "3.2.14"
+  private val scalaTestPlusCheckVersion   = "3.2.14.0"
+  private val scalaTestPlusMockitoVersion = "3.2.14.0"
+  private val scalaTestPlusPlayVersion    = "5.1.0"
   private val flexMarkVersion = "0.64.0"
 
   private val compile = Seq(
@@ -29,7 +29,7 @@ object Dependencies {
     "com.typesafe.akka" %% "akka-testkit" % PlayVersion.akkaVersion % Test,
     "org.scalatest" %% "scalatest" % scalaTestVersion % Test,
     "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusPlayVersion % Test,
-    "org.scalatestplus" %% "mockito-4-5" % scalaTestPlusMockitoVersion % Test,
+    "org.scalatestplus" %% "mockito-4-6" % scalaTestPlusMockitoVersion % Test,
     "org.scalatestplus" %% "scalacheck-1-17" % scalaTestPlusCheckVersion % Test,
     "com.vladsch.flexmark" % "flexmark-all" % flexMarkVersion % Test // for scalatest 3.2.x
   )
